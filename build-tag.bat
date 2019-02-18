c1b5047909c138335ebd05853b2ba4a5bc9e0856
Takeshi Abe <tabe@fixedpoint.jp>
2019-02-18 15:28:07 +0900

    Turn echo on

--- build-tag.bat
+++ build-tag.bat
@@ -1,2 +1,2 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 cl tag.c
